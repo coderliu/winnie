0de15c95a146f1150f82cd1cdd99e5596d1fc438
coderliu <coderliu@hotmail.com>
2017-07-16 18:31:26 +0800

    save rich text of posts

--- app/services/data_source_service.rb
+++ app/services/data_source_service.rb
@@ -16,7 +16,7 @@ class DataSourceService
       if title.include?('招聘') && !title.include?('中学') && !title.include?('结果') && !Post.where(district: :siming, url: url).first
         content_reqs = Net::HTTP.get_response(URI.parse(url))
         content_doc = Nokogiri::HTML(content_reqs.body)
-        content = content_doc.css('.exam_content').first.text
+        content = content_doc.css('.exam_content').first.to_s
         Post.create(title: title, url: url, content: content, include_english: content.include?('英语'), district: :siming)
       end
     end
